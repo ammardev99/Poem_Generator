b5e17aee0786b5e5cc9d6bfe76f0576691fc3759
Ammar Shafiq <Ammardev99@gmail.com>
2024-05-31 17:17:51 +0500

    set aboutus page

--- lib/modules/aboutus/view.dart
+++ lib/modules/aboutus/view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:poem_generator/components/assets.dart';
+import 'package:poem_generator/components/widgets.dart';
 
 import 'logic.dart';
 
@@ -11,6 +13,76 @@ class AboutusPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: BackButton(
+            color: whiteColor,
+          ),
+          title: myHeading("About Us", whiteColor),
+          centerTitle: true,
+          backgroundColor: primaryColor,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                sizeBox(20),
+                myTitle(
+                  "Welcome to Poem  Generator",
+                  secondaryColor,
+                ),
+                myHeading(
+                    "where poetry meets innovation. We are passionate creators dedicated to revolutionizing the world of poetry through the power of artificial intelligence."),
+                sizeBox(5),
+                Divider(
+                  color: primaryColor03,
+                ),
+                sizeBox(5),
+                aboutCard("Our Mission",
+                    'Our mission is to inspire creativity and facilitate self-expression through poetry. We believe that everyone has a unique voice waiting to be heard, and our AI-based Poem Generator empowers users to unleash their creativity without limitations.'),
+                aboutCard("What We Do",
+                    'Using cutting-edge natural language processing algorithms and machine learning techniques, we\'ve developed an intelligent Poem Generator that can craft original poetry in various styles and forms. Whether you\'re a seasoned poet seeking inspiration or a novice exploring the world of verse, our app provides a platform for endless creativity.'),
+                aboutCard("Meet the Team",
+                    'Behind Poem Generator is TheWebConcept. TheWebConcept is a team of dedicated individuals passionate about literature, technology, and creativity. We bring together expertise in AI, linguistics, and poetry to create an app that pushes the boundaries of what\'s possible.'),
+                sizeBox(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: nextButtonStyle(context),
+                      child: myHeading("More Apps", whiteColor),
+                    ),
+                  ],
+                ),
+                sizeBox(40),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+}
+
+Widget aboutCard(String title, String text) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      sizeBox(10),
+      myTitle(
+        title,
+        secondaryColor,
+      ),
+      info(text),
+      sizeBox(5),
+      Divider(
+        color: primaryColor03,
+      ),
+    ],
+  );
 }
--- lib/modules/menu/view.dart
+++ lib/modules/menu/view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:poem_generator/Screens/ineer%20screens/aboutus.dart';
 import 'package:poem_generator/Screens/ineer%20screens/contactus.dart';
 import 'package:poem_generator/Screens/onboarding/onboarding.dart';
 import 'package:poem_generator/components/widgets.dart';
+import 'package:poem_generator/modules/aboutus/view.dart';
 import 'package:poem_generator/modules/editprofile/view.dart';
 import 'package:poem_generator/modules/help/view.dart';
 
@@ -28,7 +28,7 @@ class MenuPage extends StatelessWidget {
           shareOption(Icons.share,'Share This App','share',),
           redirectOption(Icons.devices_other,'Other Apps','https://play.google.com/store/apps/developer?id=samz+creation&hl=en&gl=US'),
           menuOption(Icons.help_outline,'Help',context, HelpPage()),
-          menuOption(Icons.info_outline,'About us',context,const AboutUs()),
+          menuOption(Icons.info_outline,'About us',context, AboutusPage()),
           menuOption(Icons.support_agent,'Contact us',context,const ContactUs()),
           menuOption(Icons.logout, 'Log out', context, const OnBoarding()),
         ],
